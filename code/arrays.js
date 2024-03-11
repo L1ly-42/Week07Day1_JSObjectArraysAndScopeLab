f5e6137cf5983342271d6cec24829187426d39dd
Leila Peltier <l_peltier@outlook.com>
2024-03-11 14:14:58 +0000

    answered Q5 in arrays.js

--- code/arrays.js
+++ code/arrays.js
@@ -35,16 +35,13 @@ console.log("There are " + stationCount + " stations.");
 thirdStation = stations[2];
 console.log("The third station is " + thirdStation);
 
-
-
 /**
  * Q5.
  * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
  */
 
-// console.log(stations);
-
-
+stations[1] = "Berwick-upon-Tweed";
+console.log(stations);
 
 /**
  * Q6.
